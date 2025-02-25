021389fa239224785b67fa217c549ca725184787
valera-pzc <starcoship09@gmail.com>
2025-02-25 13:45:07 +0000

    routes n games final

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//routes and games
 Route::get('/games', [ProductController::class, 'index'])->name('games.index');
 Route::get('/games/{id}', [ProductController::class, 'show'])->name('games.show');
